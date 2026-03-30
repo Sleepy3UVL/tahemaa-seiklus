ed376acf8fa21f22a41942003a43601b359eb164
Sleepy3UVL <carl2.tilgor@gmail.com>
2026-03-30 08:32:29 +0300

    Fix mobile fullscreen layout

--- src/game.js
+++ src/game.js
@@ -6,11 +6,23 @@ import ChooseWordScene from './scenes/ChooseWordScene';
 
 const config = {
   type: Phaser.AUTO,
-  width: 900,
-  height: 600,
+
   backgroundColor: '#d0f4ff',
   parent: 'game-container',
-  scene: [StartScene, FindLetterScene, BuildWordScene, ChooseWordScene]
+
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 900,
+    height: 600
+  },
+
+  scene: [
+    StartScene,
+    FindLetterScene,
+    BuildWordScene,
+    ChooseWordScene
+  ]
 };
 
-new Phaser.Game(config);
+const game = new Phaser.Game(config);
